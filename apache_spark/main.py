fa61fab9785098c6c8947cf22c18b56c2461e379
Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>
2023-08-02 17:10:19 +0300

    crossjoin -> adj matrix

--- apache_spark/main.py
+++ apache_spark/main.py
@@ -1,45 +1,54 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-import sys
-from random import random
-from operator import add
-
+import numpy as np
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, split, explode, udf, array, collect_list
+from pyspark.sql.types import StringType, ArrayType, FloatType
 
+from time import time
+from os import listdir
+
+spaceDeleteUDF = udf(lambda s: s.replace(" ", ""), StringType())
+
+
+def outer_function(a):
+    return 2*a
+    #return np.outer(a, a)
+
+
+outerUDF = udf(outer_function, ArrayType(FloatType()))
 
 if __name__ == "__main__":
     """
-        Usage: pi [partitions]
+        Usage: 1.word counts on documents
+               2.adj matrix creation
     """
-    spark = SparkSession\
-        .builder\
-        .appName("PythonPi")\
+    spark = SparkSession \
+        .builder \
+        .master("local") \
+        .appName("Word_Count") \
         .getOrCreate()
 
-    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-    n = 100000 * partitions
+    sc = spark.sparkContext
 
-    def f(_: int) -> float:
-        x = random() * 2 - 1
-        y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
-
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+    files = listdir("data/Dummy_docs")
+    start = time()
+    for filename in files:
+        df = spark.read.text(f"data/Dummy_docs/{filename}")
+        df = df.withColumn('Word', spaceDeleteUDF('value'))
+        df_count = (df.withColumn('word', explode(split(col('Word'), ' ')))
+                    .groupby('word')
+                    .count()
+                    .withColumn('count2',col('count'))
+                    )
+        #df_count.show()
+        col1 = df_count.select('word','count')
+        #col1.show()
+        col2 = df_count.select('count2')
+        #col2.show()
+        matrix = col1.crossJoin(col2)
+        #matrix.show()
+        matrix_to_df = matrix.withColumn('crossprod', col('count')*col('count2')).groupby('word').agg(collect_list('crossprod'))
+        #matrix_to_df.show()
 
     spark.stop()
+    end = time()
+    print(f"***----->Execution time :{end - start}")
